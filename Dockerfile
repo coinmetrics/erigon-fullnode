1e2cefe7c54ce496a96d22a7c963f8a799eacc81
Marko Sabec <marko@coinmetrics.io>
2021-08-06 13:30:58 +0200

    2021.08.01

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Build image
+FROM alpine:latest AS build
+
+ARG VERSION
+
+RUN apk --no-cache add make gcc g++ linux-headers git bash ca-certificates libgcc libstdc++ alpine-sdk go
+RUN git clone --depth 1 -b v${VERSION} https://github.com/ledgerwatch/erigon /opt/erigon
+
+WORKDIR /opt/erigon
+
+RUN make erigon rpcdaemon integration sentry
+
+
+# Final image
+FROM alpine:latest
+
+RUN apk add --no-cache ca-certificates libgcc libstdc++ tzdata
+COPY --from=build /opt/erigon/build/bin/* /usr/local/bin/
+
+RUN adduser -D -u 1000 runner
+USER runner
+WORKDIR /home/runner
+
+USER runner
+
+EXPOSE 8545 8546 30303 30303/udp 30304 30304/udp 8080 9090 6060
+
+ENTRYPOINT ["erigon"]
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+export DOCKER_REGISTRY_REPO="${DOCKER_REGISTRY_REPO:-"$(basename "$PWD")"}"
+
+export VERSION=$(cat version.txt)
+echo "Building $DOCKER_REGISTRY_REPO version ${VERSION}."
+
+echo "Building image..."
+docker build --build-arg "VERSION=${VERSION}" -t "${DOCKER_REGISTRY_REPO}:${VERSION}" .
+echo "Image ready."
+
+if [ -n "${DOCKER_REGISTRY}" ] && [ -n "${DOCKER_REGISTRY_USER}" ] && [ -n "${DOCKER_REGISTRY_PASSWORD}" ]
+then
+	echo "Logging into ${DOCKER_REGISTRY}..."
+	docker login -u="${DOCKER_REGISTRY_USER}" -p="${DOCKER_REGISTRY_PASSWORD}" "${DOCKER_REGISTRY}"
+	echo "Pushing image to ${DOCKER_REGISTRY_REPO}:${VERSION}..."
+	docker push "${DOCKER_REGISTRY_REPO}:${VERSION}"
+fi
